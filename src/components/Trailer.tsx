2c3f76ba6b0b0953c974dc8155502dd3c24a0704
glsndroj <galsandooorj@gmail.com>
2025-10-14 16:18:44 +0800

    delete dialog

--- src/components/Trailer.tsx
+++ src/components/Trailer.tsx
@@ -3,11 +3,7 @@ import ReactPlayer from 'react-player'
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
-  DialogClose,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
