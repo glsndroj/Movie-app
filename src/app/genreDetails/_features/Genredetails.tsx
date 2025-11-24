eb71f201d4aa3d3ffe2ef829a2f5cf30a7af29fb
glsndroj <galsandooorj@gmail.com>
2025-11-24 17:41:14 +0800

    genredetails skeleton hiij duusgah

--- src/app/genreDetails/_features/Genredetails.tsx
+++ src/app/genreDetails/_features/Genredetails.tsx
@@ -1,47 +1,44 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import GenreSkeleton from "./genreSkeleton";
 import { Movie } from "@/components/Movie";
+import { Movietype } from "@/lib/typeofmovies";
 import { axiosInstance } from "@/lib/utils";
 
-type GenrePageProps = {
-  searchParams: {
-    genreId: string;
-    genreName?: string;
-    page?: string;
-  };
+type genrePageProps = {
+    searchParams: {genreId: string; genreName?: string; page?: string}
 };
 
-export default function Genredetails({ searchParams }: GenrePageProps) {
-  const { genreId, genreName, page = "1" } = searchParams;
-  const [movies, setMovies] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
+export async function Genredetails ({searchParams}: genrePageProps)  {
+    
+    const {genreId, genreName, page = "1"}= searchParams;
+    const safePage = Math.max(1, Math.min(500, parseInt(page)));
+    
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      setLoading(true);
-      const response = await axiosInstance.get(
-        `/discover/movie?language=en&with_genres=${genreId}&page=${page}`
-      );
-      setMovies(response.data.results);
-      setLoading(false);
-    };
-    fetchMovies();
-  }, [genreId, page]);
+    const getMoviesByGenre = async () => {
+        const response = await axiosInstance.get(`/discover/movie?language=en&with_genres=${genreId}&page=${page}`);
+        console.log(response.data)
+        return response.data;
+    }
+    
+    const movies = await getMoviesByGenre()
+    
+    return(
 
-  if (loading) return <GenreSkeleton />;
-
-  return (
-    <div>
-      <h4 className="p-2 font-bold text-[24px]">
-        {movies.length} Titles {genreName ? ` in "${genreName}"` : ""}
-      </h4>
-      <div className="flex gap-5 w-[1300px] flex-wrap">
-        {movies.map((movie) => (
-          <Movie key={movie.id} movie={movie} />
-        ))}
-      </div>
-    </div>
-  );
+        <>
+        <div >
+            <h4 className="p-2 font-bold text-[24px]">
+            {movies.total_results} Titles{genreName ? ` in "${genreName}"` : ""}</h4>
+            
+           <div className="flex gap-5 w-[1300px] flex-wrap">
+           {movies.results.map((movie: Movietype) => {
+                return (
+                <Movie key={movie.id} 
+               movie={movie}  
+                />
+            );
+                
+            })} 
+           </div>
+        </div>
+        </>
+        
+    )
 }
--- src/app/genreDetails/page.tsx
+++ src/app/genreDetails/page.tsx
@@ -1,23 +1,22 @@
 import { Genres } from "@/components/Genres";
-import Genredetails from "./_features/Genredetails";
+import { Genredetails } from "./_features/Genredetails";
 import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
 import { PaginationDemo } from "./_features/Pagination";
+import { Suspense } from "react";
+import GenreSkeleton from "./_features/genreSkeleton";
 
-type GenrePageProps = {
+type genrePageProps = {
   searchParams: { genreId: string; genreName?: string; page?: string };
 };
 
-export default async function Home({ searchParams }: GenrePageProps) {
-  
-  const { genreId, genreName, page } = searchParams;
-
+export default function Home({ searchParams }: genrePageProps) {
   return (
     <>
       <div className="px-20 mt-10 text-3xl font-bold">
         <span>Search filter</span>
       </div>
       <div className="flex w-full gap-5 px-20 py-10">
-        <div className="flex flex-col w-[600px] h-fit gap-7 rounded-md mt-2">
+        <div className="flex flex-col w-[600px] h-fit gap-7  rounded-md mt-2 ">
           <div className="flex flex-col gap-6 border-b-[2px]">
             <p className="text-3xl font-bold">Genres</p>
             <p>See lists of movies by genre</p>
@@ -27,10 +26,11 @@ export default async function Home({ searchParams }: GenrePageProps) {
         </div>
 
         <div>
-          <Genredetails searchParams={{ genreId, genreName, page }}/>
+          <Suspense fallback={<GenreSkeleton/>}>
+            <Genredetails searchParams={searchParams} />
+          </Suspense>
         </div>
       </div>
-
       <div>
         <PaginationDemo />
       </div>
